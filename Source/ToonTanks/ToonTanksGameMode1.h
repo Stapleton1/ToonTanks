2ac13d228ca35a48c4a0223e1eff0f7deb3e3cdb
Erdal İnceler <erdalinceler1@gmail.com>
2023-03-27 10:58:07 +0300

    ToonTanks
    git commit

---
+++ Source/ToonTanks/ToonTanksGameMode1.h
@@ -0,0 +1,30 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ToonTanksGameMode1.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TOONTANKS_API AToonTanksGameMode1 : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+public:
+
+	void ActorDied(AActor* DeadActor);
+
+protected:
+
+	virtual void BeginPlay() override;
+
+private:
+
+	class ATank* Tank;
+
+
+};
